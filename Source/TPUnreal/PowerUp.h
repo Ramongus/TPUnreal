fc1d33537870c3c9fa7b2dff70cebed65dcfccb9
Ramiro Oliva <oliva.ramiro2@gmail.com>
2020-07-06 20:41:59 -0300

    Principio-PowerUp

---
+++ Source/TPUnreal/PowerUp.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine.h"
+#include "Engine/World.h"
+#include "CoreMinimal.h"
+#include "MyPlayer.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "PowerUp.generated.h"
+
+UCLASS()
+class TPUNREAL_API APowerUp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APowerUp();
+
+	UPROPERTY(EditAnywhere)
+		FComponentReference reference;
+
+	UPROPERTY()
+		USphereComponent* sphereCol;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+		void OverlapDemo(UPrimitiveComponent* primComp, AActor* overlapedActor, UPrimitiveComponent* otherOverlapedComponent, int32 overInt, bool overBool, const FHitResult& overHit);
+
+};
